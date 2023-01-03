0472915d17396d1a501f056418a25be8e6106f16
Nazarii Luhovyi <luhovyinazarii@gmail.com>
2023-01-03 15:20:23 +0100

    init: work functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
+
+  // function reverse (n) {
   
+  n = String(n);
+  let i = 0;
+  let result = '';
+  let minus = '';
+  while (i < n.length) { 
+    if (n[i] === '-') {
+      minus = '-';
+    } else {
+    result = n[i] + result;
+    }
+    i++;
+  }
+
+// return +(minus + result);
+// return n;
+return +(result);
 }
+
+
+// console.log(reverse(343355));
